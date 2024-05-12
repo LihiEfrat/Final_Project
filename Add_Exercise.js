07438f5e9ebbab89ef1eb657480153d6a73e4185
Lihie <lihi729@gmail.com>
2024-05-12 15:16:39 +0300

    Added new files - Add_Exercise.js and Exercise_Manager.js

---
+++ Add_Exercise.js
@@ -0,0 +1,197 @@
+// Screen2.js
+
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+const data = [
+    { label: 'כתף', value: '1' },
+    { label: 'ברך', value: '2' },
+    { label: 'רגל', value: '3' },
+    { label: 'גב', value: '4' },
+    { label: 'קרסול', value: '5' },
+    { label: 'צוואר', value: '6' },
+    { label: 'כף יד', value: '7' },
+    { label: 'שכמה', value: '8' },
+];
+
+const Screen2 = () => {
+    const [text, setText] = useState("")
+    const [value, setValue] = useState(null);
+    const [isFocus, setIsFocus] = useState(false);
+    const [isChecked, setIsChecked] = useState(false);
+
+    return (
+        <SafeAreaView>       
+            <View style={styles.mainContainer}>
+                <Text style={styles.titleText}> הוספת תרגיל למאגר</Text>
+                <TextInput
+                    defaultValue='שם התרגיל'
+                    onChangeText={txt => setText(txt)}
+                    style={[styles.input, { height: 40 }]}
+                />
+
+                <Dropdown
+                    style={[styles.dropdown, isFocus && { borderColor: 'blue' }]}
+                    placeholderStyle={styles.placeholderStyle}
+                    selectedTextStyle={styles.selectedTextStyle}
+                    inputSearchStyle={styles.inputSearchStyle}
+                    inputSearchStyle={[styles.inputSearchStyle, { textAlign: 'right' }]}
+                    data={data}
+                    search
+                    maxHeight={300}
+                    labelField="label"
+                    valueField="value"
+                    placeholder={!isFocus ? 'בחר/י קטגוריה' : '...'}
+                    searchPlaceholder="חפש/י..."
+                    value={value}
+                    onFocus={() => setIsFocus(true)}
+                    onBlur={() => setIsFocus(false)}
+                    onChange={item => {
+                        setValue(item.value);
+                        setIsFocus(false);
+                    }}
+                />
+
+                <TextInput
+                    defaultValue='פירוט התרגיל'
+                    onChangeText={txt => setText(txt)}
+                    multiline={true}
+                    style={[styles.input, { height: 100, paddingTop: 10 }]}
+                />
+
+                <View style={styles.uploadContainer}>
+                    <TouchableOpacity
+                        style={styles.uploadButton}
+                        onPress={() => console.log('Button pressed for uploading file')}
+                    >
+                        <AntDesign name="upload" size={24} color="white" />
+                    </TouchableOpacity>
+                    <Text style={{ color: 'black', fontSize: 16 }}>העלאת סרטון תרגיל</Text>
+                </View>
+
+                <View style={styles.checkboxContainer}>
+                    <TouchableOpacity
+                        style={[styles.checkbox, isChecked && { backgroundColor: '#19a6b8' }]}
+                        onPress={() => setIsChecked(!isChecked)}
+                    >
+                        {isChecked && <AntDesign name="check" size={20} color="white" />}
+                    </TouchableOpacity>
+                    <Text style={{ color: 'black', fontSize: 16 }}>אישור הורדת התרגיל ותכניו במצב לא מקוון</Text>
+                </View>
+
+                <TouchableOpacity style={styles.saveButton} onPress={() => console.log('button pressed for saving changes')}>
+                    <Text style={{ color: 'white'}}> שמור שינויים </Text>
+                </TouchableOpacity>
+
+            </View>
+        </SafeAreaView>
+    );
+}
+
+export default Screen2;
+
+const styles = StyleSheet.create({
+    titleText: {
+        fontSize: 16, 
+        textAlign: 'right',
+        padding: 15,
+    },
+
+    mainContainer: {
+        backgroundColor: '#d8f6fa',
+        minHeight: '100%',
+        paddingBottom: 300,
+        textAlign: 'right'
+    },
+
+    input: {
+        borderWidth: 0.5,
+        borderColor: 'gray',
+        borderRadius: 8,
+        paddingHorizontal: 8,
+        marginHorizontal: 20,
+        textAlign: 'right',
+        fontSize: 16
+    },
+
+    dropdown: {
+        height: 50,
+        borderColor: 'gray',
+        borderWidth: 0.5,
+        borderRadius: 8,
+        paddingHorizontal: 8,
+        margin: 20
+    },
+
+    label: {
+        position: 'absolute',
+        backgroundColor: 'white',
+        left: 22,
+        top: 8,
+        zIndex: 999,
+        paddingHorizontal: 8,
+        fontSize: 14,
+    },
+
+    placeholderStyle: {
+        fontSize: 16,
+    },
+
+    selectedTextStyle: {
+        fontSize: 16,
+    },
+ 
+    inputSearchStyle: {
+        height: 40,
+        fontSize: 16,
+    },
+
+    uploadContainer: {
+        flexDirection: 'row-reverse',
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+
+    uploadButton: {
+        backgroundColor: '#19a6b8',
+        width: 50,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 8,
+        marginRight: 20,
+        marginLeft: 10,
+        marginTop: 20,
+    },
+
+    checkboxContainer: {
+        flexDirection: 'row-reverse',
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+
+    checkbox: {
+        width: 20,
+        height: 20,
+        borderWidth: 1,
+        borderColor: '#19a6b8',
+        borderRadius: 3,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 20,
+        marginLeft: 10,
+    },
+
+    saveButton: {
+        margin: 20,
+        backgroundColor: '#19a6b8',
+        width: 100,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 8,
+        alignSelf: 'center',
+    },
+});
---
+++ Exercise_Manager.js
@@ -0,0 +1,115 @@
+// Screen1.js
+
+import React from 'react';
+import { View, Text, TouchableOpacity, SafeAreaView, ScrollView, FlatList, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+// Exercise data
+const exerciseData = [
+    { id: '1', title: 'Exercise 1', /* Other exercise data */ },
+    { id: '2', title: 'Exercise 2', /* Other exercise data */ },
+    { id: '3', title: 'Exercise 3', /* Other exercise data */ },
+    { id: '4', title: 'Exercise 4', /* Other exercise data */ },
+    { id: '5', title: 'Exercise 5', /* Other exercise data */ },
+    { id: '6', title: 'Exercise 6', /* Other exercise data */ },
+];
+
+// ExerciseItem component to render each exercise
+const ExerciseItem = ({ item }) => {
+    return (
+        <View style={styles.exerciseItem}>
+            {/* Display exercise content */}
+            <View style={styles.videoClip}></View>
+            <Text style={{ textAlign: 'center', width: 100 }}>{item.title}</Text>
+        </View>
+    );
+};
+
+
+const Screen1 = () => {
+    const navigation = useNavigation();
+
+    return (
+        <SafeAreaView style={{flex: 1}}>
+            <View style={{padding: 10}}>
+                <Text style={{textAlign: 'right'}}>דף ראשי - מאגר תרגילים</Text>
+            </View>
+
+            <View style={styles.titleContainer}>
+                <Text style={styles.titleText}>מאגר תרגילים</Text>
+            </View>
+               
+            <TouchableOpacity style={styles.addButton} onPress={() => navigation.navigate('Add_Exercise')}>
+                <Text style={{ textAlign: 'center', color: 'white' }}>הוספת תרגיל למאגר</Text>
+            </TouchableOpacity>
+
+            <ScrollView style={{ flex: 1 }}>
+                <View style={styles.sectionTitle}>
+                    <Text style={styles.sectionTitleText}>כל התרגילים</Text>
+                </View>
+
+                {/* Exercise List */}
+                <FlatList
+                    data={exerciseData}
+                    renderItem={({ item }) => <ExerciseItem item={item} />}
+                    keyExtractor={item => item.id}
+                    numColumns={3} 
+                />
+
+            </ScrollView>
+        </SafeAreaView>
+    );
+}
+
+export default Screen1;
+
+const styles = StyleSheet.create({
+    titleContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#19a6b8',
+    },
+
+    titleText: {
+        color: 'white',
+        fontSize: 30,
+        margin: 10,
+    },
+
+    addButton: {
+        margin: 20,
+        backgroundColor: '#19a6b8',
+        width: 100,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 8,
+        alignSelf: 'center',
+    },
+
+    sectionTitle: {
+        backgroundColor: '#19a6b8',
+        padding: 5,
+    },
+
+    sectionTitleText: {
+        color: 'white',
+        fontSize: 20,
+        textAlign: 'right',
+        alignItems: 'center',
+    },
+
+    exerciseItem: {
+        flex: 1,
+        marginHorizontal: 5,
+        marginBottom: 10,
+    },
+
+    videoClip: {
+        width: 100,
+        height: 100,
+        backgroundColor: 'lightgray', // Placeholder for video clip
+        marginBottom: 5,
+    },
+});
+
