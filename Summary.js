eb0bf3c148bdd2d4afe1656db59613b29099809b
Edend258 <82519131+Edend258@users.noreply.github.com>
2024-05-18 19:47:47 +0300

    Add files via upload

---
+++ Summary.js
@@ -0,0 +1,77 @@
+
+
+import React from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+const Summary = ({ route }) => {
+  const { programName, exerciseData } = route.params;
+
+  // Filter the exercise data to include only items with value > 0
+  const filteredExerciseData = exerciseData.map(area => {
+    const filteredExercises = area.exerciseOp.filter(exercise => exercise.value > 0);
+    return { ...area, exerciseOp: filteredExercises };
+  }).filter(area => area.exerciseOp.length > 0); // Remove areas with no valid exercises
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>סיכום</Text>
+      <Text style={styles.programName}>שם התוכנית: {programName}</Text>
+      <FlatList
+        data={filteredExerciseData}
+        keyExtractor={(item) => item.areaName}
+        renderItem={({ item }) => (
+          <View style={styles.areaContainer}>
+            <Text style={styles.areaName}>{item.areaName}</Text>
+            {item.exerciseOp.map((exercise, index) => (
+              <View key={index} style={styles.exerciseContainer}>
+                <Text>{exercise.name}: {exercise.value}</Text>
+              </View>
+            ))}
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+export default Summary;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    direction: 'rtl',
+    textAlign: 'right',
+    alignItems:'flex-start' // Align text to the right
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    direction: 'rtl',
+    textAlign: 'right', 
+    // Align text to the right
+  },
+  programName: {
+    fontSize: 18,
+    marginBottom: 20,
+    direction: 'rtl',
+    textAlign: 'right', // Align text to the right
+  },
+  areaContainer: {
+    marginBottom: 20,
+    direction: 'rtl',
+  },
+  areaName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    direction: 'rtl',
+    textAlign: 'right', // Align text to the right
+  },
+  exerciseContainer: {
+    marginLeft: 10,
+    direction: 'rtl',
+    textAlign: 'right', // Align text to the right
+  },
+});
