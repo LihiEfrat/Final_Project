e05bdc61a3bc99ce20ab77233b3309c3b69f2f88
Nitzanlo <nitzan1801@gmail.com>
2024-05-31 12:09:58 +0300

    Delete my_django_project directory

--- my_django_project/my_project/myapp/admin.py
+++
@@ -1,8 +0,0 @@
-# myapp/admin.py
-from django.contrib import admin
-from .models import Exercise
-
-# Register your models here.
-
-# This will make your Exercise model visible in the Django admin interface.
-admin.site.register(Exercise)
